2ec13ec1cdc16ba422d7553035c49bcbd09bfc1a
lanemccarty <lane.mccarty@tufts.edu>
2020-04-14 18:08:27 -0400

    Update KMeansClustering_driver.py

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -1,2 +1,14 @@
 #Please place your FUNCTION code for step 4 here.
+
+
 import KMeansClustering_functions as kmc #Use kmc to call your functions
+
+#Number of clusters 
+k=2
+
+glucose, hemoglobin, classification = kmc.openckdfile()
+glucose, hemoglobin, classification = kmc.normalizeData(glucose, hemoglobin, classification)
+assignment, centroids, glucose, hemoglobin, classification = kmc.iterate(k,70)
+kmc.graphingKMeans(glucose, hemoglobin, assignment, centroids)
+kmc.calculate(classification, assignment)
+
